9d51db999cf85ff9a6278b0c5e074eab01463c50
akinsanyaleke <38169420+akinsanyaleke@users.noreply.github.com>
2022-04-04 23:13:22 -0400

    Update create.sh

--- scripts/create.sh
+++ scripts/create.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DIR="https://raw.githubusercontent.com/ccollicutt/spring-cloud-gateway-learning-center/main/resources/"
+DIR="https://raw.githubusercontent.com/akinsanyaleke/cb/main/resources"
 
 kubectl apply -f $DIR/workshop.yaml 2>/dev/null
 kubectl apply -f $DIR/training-portal.yaml 2>/dev/null
